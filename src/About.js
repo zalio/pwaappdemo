bda116f2a9e957f35be38bcf248eb7a785854607
zalio <orulluoglu.zeynel@gmail.com>
2020-08-25 16:02:27 +0300

    package json edits

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='container'>
+      <h5>About Us</h5>
+      <p>
+        merhaba benim adım name
+      </p>
+    </div>
+  );
+};
+
+export default About;
