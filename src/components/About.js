5768dd2868e48184635e7a25ec44574c9818088b
zalio <orulluoglu.zeynel@gmail.com>
2020-08-26 14:05:58 +0300

    local storage is added

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const About = () => {
+  const [name, setname] = useState('')
+  const [getting, setgetting] = useState('')
+  const saveName = (name) => {
+    if (name !== '')
+      localStorage.setItem('name', name);
+    console.log(name);
+  };
+  useEffect(() => {
+    console.log('girdi')
+    const item = localStorage.getItem('name');
+    setgetting(item);
+    console.log(item)
+  }, [name])
   return (
     <div className='container'>
       <h5>About Us</h5>
       <p>
         merhaba benim adım name
       </p>
+      <input value={name} onChange={e => setname(e.target.value)}></input>
+      <button onClick={saveName(name)}>Isim yaz</button>
+      <p>{getting}</p>
     </div>
   );
 };
